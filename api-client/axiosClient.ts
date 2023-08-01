8d654db51068d22dbd3faf9b112c2a63bd97513f
nhudinhdd <nhudinhan03051998@gmail.com>
2023-08-01 21:08:23 +0700

    update env

--- api-client/axiosClient.ts
+++ api-client/axiosClient.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const axiosClient = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: process.env.DOMAIN + "/api",
   headers: {
     "Content-Type": "application/json",
   },
--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   images: { domains: ["game-addict.s3.ap-northeast-2.amazonaws.com"] },
+  env: {
+    DOMAIN: process.env.DOMAIN,
+  },
 };
 
 module.exports = nextConfig;
